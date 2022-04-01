d32fb2046df1266eb43d9dde08c7f2b415382a44
Lore <74215708+trwa@users.noreply.github.com>
2022-04-01 17:02:47 +0200

    factor out old class hierarchy

--- app/headers/rin_maker.h
+++ app/headers/rin_maker.h
@@ -19,7 +19,8 @@
 
 #include "spatial/kdtree.h"
 
-namespace rin_maker {
+namespace rin_maker
+{
 
 namespace fs = std::filesystem;
 
@@ -66,72 +67,4 @@ public:
 
     rin::graph operator()(parameters::interaction_type interaction_type, parameters::policy network_policy) const;
 };
-
-////////////////////////////////////////////////////////////////////////
-
-class base {
-protected:
-    vector<aminoacid*> _aminoacids;
-    network _rin_network;
-
-    explicit base(fs::path const& pdb_path);
-
-public:
-    virtual ~base() {
-        for (auto* res: _aminoacids)
-            delete res;
-    }
-
-    rin::graph get_graph(parameters::interaction_type inter) const;
-};
-
-class all_bonds final : public base {
-private:
-    double _query_dist_hbond, _query_dist_vdw, _query_dist_ionic, _query_dist_pipi, _query_dist_pica;
-
-    kdtree<atom, 3> _hdonor_tree, _vdw_tree;
-    vector<atom const*> _hacceptor_vector, _vdw_vector, _cation_vector;
-
-    kdtree<ring, 3> _ring_tree, _pication_ring_tree;
-    vector<ring const*> _ring_vector, _pication_ring_vector;
-
-    kdtree<ionic_group, 3> _positive_ion_tree;
-    vector<ionic_group const*> _negative_ion_vector;
-
-public:
-    explicit all_bonds(fs::path const& pdb_path);
-};
-
-typedef function<atom const*(aminoacid const&)> const& carbon_getter;
-
-class backbone : public base {
-private:
-    double _query_dist_carbon;
-
-protected:
-    kdtree<atom, 3> _carbon_tree;
-    vector<chemical_entity::atom const*> _carbon_vector;
-
-    explicit backbone(fs::path const& pdb_path, carbon_getter getter);
-};
-
-class alpha_carbon final : public backbone {
-public:
-    explicit alpha_carbon(fs::path const& pdb_path)
-            : backbone(
-            pdb_path, [](aminoacid const& res) -> atom const* {
-                return res.ca();
-            }) { lm::main()->info("alpha carbons: {}", _carbon_vector.size()); }
-};
-
-class beta_carbon final : public backbone {
-public:
-    explicit beta_carbon(fs::path const& pdb_path)
-            : backbone(
-            pdb_path, [](aminoacid const& res) -> atom const* {
-                return res.cb();
-            }) { lm::main()->info("beta carbons: {}", _carbon_vector.size()); }
-};
-
-unique_ptr<base const> make_instance(parameters::policy policy, fs::path const& path);
 }
